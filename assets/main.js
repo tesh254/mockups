f95c193c23ed264a1449798d7b63d9aa9078b5fd
tesh254 <ewachira254@gmail.com>
2019-01-25 10:40:19 +0300

    this is main

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,31 @@
-$(function(){
-	$("#login").click(function(){
-		$(".login").modal('show');
-	});
-	$(".login").modal({
-		closable: true
-	});
+// Login modal
+$(function() {
+  $("#login").click(function() {
+    $(".login").modal("show");
+  });
+  $(".login").modal({
+    closable: true
+  });
 });
 
-$(function(){
-	$("#signup").click(function(){
-		$(".signup").modal('show');
-	});
-	$(".signup").modal({
-		closable: true
-	});
+// Signup modal
+$(function() {
+  $("#signup").click(function() {
+    $(".signup").modal("show");
+  });
+  $(".signup").modal({
+    closable: true
+  });
+});
+
+// Dropdown menu setup
+$(document).ready(function() {
+  $(".ui.dropdown").dropdown();
+});
+
+$(document).ready(function() {
+  $(".rating").rating({
+    initialRating: 0,
+    maxRating: 5
+  });
 });
