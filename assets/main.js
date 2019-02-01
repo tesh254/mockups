be10952ce5089a5a84858016c76b29de5cdafb3a
Bolton <bolton.otieno@gmail.com>
2019-02-01 09:08:35 +0300

    favorite, sharing, average rate, like-dislike comment, tags

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,6 @@
 // Login modal
-$(function() {
-  $("#login").click(function() {
+$(function () {
+  $("#login").click(function () {
     $(".login").modal("show");
   });
   $(".login").modal({
@@ -9,8 +9,8 @@ $(function() {
 });
 
 // Signup modal
-$(function() {
-  $("#signup").click(function() {
+$(function () {
+  $("#signup").click(function () {
     $(".signup").modal("show");
   });
   $(".signup").modal({
@@ -19,13 +19,31 @@ $(function() {
 });
 
 // Dropdown menu setup
-$(document).ready(function() {
+$(document).ready(function () {
   $(".ui.dropdown").dropdown();
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
   $(".rating").rating({
     initialRating: 0,
     maxRating: 5
   });
+});
+
+$(function () {
+
+  $('#tags input').on('focusout', function () {
+    var txt = this.value.replace(/[^a-zA-Z0-9\+\-\.\#]/g, ''); // allowed characters list
+    if (txt) $(this).before('<span class="tag">' + txt + '</span>');
+    this.value = "";
+    // this.focus();
+  }).on('keyup', function (e) {
+    // comma|enter (add more keyCodes delimited with | pipe)
+    if (/(188|13)/.test(e.which)) $(this).focusout();
+  });
+
+  $('#tags').on('click', '.tag', function () {
+    $(this).remove();
+  });
+
 });
